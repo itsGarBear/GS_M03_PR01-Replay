d2a00aa931add330f26cc96462a5b1170fb30cc5
itsGarBear <g.smith61900@gmail.com>
2020-09-20 14:05:40 -0700

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    bool instantReplay = false;
+
+    public float restartDelay = 1f;
+    public float replayStartTime;
+
+    public GameObject completeLevelUI;
+    public GameObject commandLogUI;
+    public GameObject replayUI;
+
+    GameObject player;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().gameObject;
+        
+        replayUI.SetActive(false);
+
+        if (CommandLog.commands.Count > 0)
+        {
+            instantReplay = true;
+            replayStartTime = Time.timeSinceLevelLoad;
+        }
+    }
+
+    void Update()
+    {
+        if (instantReplay)
+        {
+            replayUI.SetActive(true);
+            RunInstantReplay();
+        }
+    }
+
+    public void CompleteLevel()
+    {
+        instantReplay = false;
+        completeLevelUI.SetActive(true);
+        replayUI.SetActive(false);
+        commandLogUI.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void RunInstantReplay()
+    {
+        if(CommandLog.commands.Count == 0)
+        {
+            instantReplay = false;
+            return;
+        }
+
+        Command command = CommandLog.commands.Peek();
+        if(Time.timeSinceLevelLoad >= command.timeStamp)
+        {
+            command = CommandLog.commands.Dequeue();
+            command._player = player.GetComponent<Rigidbody>();
+            Invoker invoker = new Invoker();
+            invoker.disableLog = true;
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Text commandsUI;
+
+    public float forwardForce = 2000f;
+    public float sideForce = 500f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            Command moveRight = new MoveRight(rb, sideForce);
+            Invoker invoker = new Invoker();
+            invoker.SetCommand(moveRight);
+            commandsUI.text += "\n" + moveRight.ToString();
+            invoker.ExecuteCommand();
+        }
+
+        if (Input.GetKey("a"))
+        {
+            Command moveLeft = new MoveLeft(rb, sideForce);
+            Invoker invoker = new Invoker();
+            invoker.SetCommand(moveLeft);
+            commandsUI.text += "\n" + moveLeft.ToString();
+            invoker.ExecuteCommand();
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
